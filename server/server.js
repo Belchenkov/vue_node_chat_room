443858aa5f87d3e6bd62ec3887ff161042c14ac2
Aleksey Belchenkov <u608110@gmail.com>
2019-02-22 06:23:05 +0300

    Settings express server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server Run on port ' + PORT);
+});
