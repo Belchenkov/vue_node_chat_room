dc8b6884e7505bd772b82f6beec1650d1129d513
Aleksey Belchenkov <u608110@gmail.com>
2019-02-22 06:59:23 +0300

    Include Socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
+io.on('connection', () => {
+    console.log('IO Connection');
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('Server Run on port ' + PORT);
 });
